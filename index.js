98e31efabecf35687c1dca9a70a9d1a9a8517e1d
PrestonFr <PrestonFr@gmail.com>
2022-06-08 19:23:16 -0400

    update

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let header = document.querySelector('#page-header')
+header.style.textAlign = "left"
 
-// Select some elements...
+let dogImages = document.querySelectorAll('.dog-image')
+for(let i = 0; i < dogImages.length; i++) {
+    dogImages[i].style.borderRadius ='50px'
+}
+
+let dogName = document.querySelectorAll('.dog-name')
+for (let i = 0; i < dogName.length; i++) {
+    dogName[i].style.textAlign = 'left'
+}
